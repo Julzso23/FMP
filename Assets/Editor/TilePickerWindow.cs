68fde40568b88a1b57583501e5ac438e6cba7113
Julian Bath <julzso23@gmail.com>
2018-03-03 01:18:51 +0000

    Added tile picker window class

---
+++ Assets/Editor/TilePickerWindow.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class TilePickerWindow : EditorWindow
+{
+    [MenuItem("Window/Tile Picker")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow<TilePickerWindow>();
+    }
+
+    private void OnGUI()
+    {
+        GameObject tileMapObject = Selection.activeGameObject;
+        if (tileMapObject)
+        {
+            TileMap tileMap = tileMapObject.GetComponent<TileMap>();
+            if (tileMap)
+            {
+                GUILayout.Label("Tile map selected.");
+            }
+            else
+            {
+                GUILayout.Label("No tile map selected.");
+            }
+        }
+        else
+        {
+            GUILayout.Label("No tile map selected.");
+        }
+    }
+
+    private void OnInspectorUpdate()
+    {
+        Repaint();
+    }
+}
