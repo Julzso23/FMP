a3a182c3fba2e94edb9f6d9600cea49ff50acfda
Julian Bath <julzso23@gmail.com>
2018-03-10 16:49:56 +0000

    Added rendering of the tile map's texture atlas on the tile picker

--- Assets/Editor/TilePickerWindow.cs
+++ Assets/Editor/TilePickerWindow.cs
@@ -3,10 +3,18 @@ using UnityEditor;
 
 public class TilePickerWindow : EditorWindow
 {
-    [MenuItem("Window/Tile Picker")]
+    [MenuItem("Window/Tile-map Editor/Tile Picker")]
     public static void ShowWindow()
     {
         EditorWindow.GetWindow<TilePickerWindow>();
+    }
+
+    private float zoom;
+    private Vector2 scrollPosition;
+
+    private void OnEnable()
+    {
+        zoom = 1f;
     }
 
     private void OnGUI()
@@ -17,21 +25,34 @@ public class TilePickerWindow : EditorWindow
             TileMap tileMap = tileMapObject.GetComponent<TileMap>();
             if (tileMap)
             {
-                GUILayout.Label("Tile map selected.");
+                RenderSpriteAtlas(tileMap);
             }
             else
             {
-                GUILayout.Label("No tile map selected.");
+                GUIUtility.ErrorLabel("You must select a tile map object.");
             }
         }
         else
         {
-            GUILayout.Label("No tile map selected.");
+            GUIUtility.ErrorLabel("You must select a tile map object.");
         }
     }
 
     private void OnInspectorUpdate()
     {
         Repaint();
+    }
+
+    private void RenderSpriteAtlas(TileMap tileMap)
+    {
+        zoom = EditorGUILayout.Slider("Zoom", zoom, 0.5f, 2f);
+
+        using (var scrollView = new EditorGUILayout.ScrollViewScope(scrollPosition))
+        {
+            Rect textureRect = EditorGUILayout.GetControlRect();
+            textureRect.width = 320f * zoom;
+            textureRect.height = 320f * zoom;
+            EditorGUI.DrawTextureTransparent(textureRect, tileMap.TextureAtlas, ScaleMode.ScaleToFit);
+        }
     }
 }
