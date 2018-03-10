553bbce5005bee095ee22e22ce8d136a973c3055
Julian Bath <julzso23@gmail.com>
2018-03-10 16:49:13 +0000

    Added sprites to the tile map class

--- Assets/Scripts/TileMap.cs
+++ Assets/Scripts/TileMap.cs
@@ -2,4 +2,14 @@ using UnityEngine;
 
 public class TileMap : MonoBehaviour
 {
+    public Sprite SpriteSelection { get; set; }
+
+    [SerializeField] private Texture2D textureAtlas;
+    public Texture2D TextureAtlas
+    {
+        get
+        {
+            return textureAtlas;
+        }
+    }
 }
