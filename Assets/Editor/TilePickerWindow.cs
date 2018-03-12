0b8a687f99e1c6b7a85bf7063ee299f010104253
Julian Bath <julzso23@gmail.com>
2018-03-12 13:32:08 +0000

    Fixed scrolling and added a selection marker

--- Assets/Editor/TilePickerWindow.cs
+++ Assets/Editor/TilePickerWindow.cs
@@ -45,25 +45,34 @@ public class TilePickerWindow : EditorWindow
 
     private void RenderSpriteAtlas(TileMap tileMap)
     {
-        zoom = EditorGUILayout.Slider("Zoom", zoom, 0.5f, 2f);
+        zoom = EditorGUILayout.Slider("Zoom", zoom, 0.5f, 4f);
 
         using (var scrollView = new EditorGUILayout.ScrollViewScope(scrollPosition))
         {
-
             Sprite[] sprites = tileMap.TextureAtlas.GetSprites();
+            tileMap.SpriteSelection = sprites[0];
 
-            Vector2 position = EditorGUILayout.GetControlRect().position;
+            EditorGUILayout.BeginHorizontal();
+
             foreach (Sprite sprite in sprites)
             {
-                Rect rect = new Rect(
+                Rect spriteRect = new Rect(
                     sprite.rect.x / sprite.texture.width,
                     sprite.rect.y / sprite.texture.height,
                     sprite.rect.width / sprite.texture.width,
                     sprite.rect.height / sprite.texture.height
                 );
-                GUI.DrawTextureWithTexCoords(new Rect(position, sprite.rect.size * zoom), tileMap.TextureAtlas, rect);
-                position += new Vector2(sprite.rect.size.x * zoom + 2f, 0f);
+                Rect rect = GUILayoutUtility.GetRect(sprite.rect.width * zoom + 4f, sprite.rect.height * zoom + 4f);
+                if (sprite == tileMap.SpriteSelection)
+                {
+                    EditorGUI.DrawRect(rect, Color.green);
+                    EditorGUI.DrawRect(new Rect(rect.position + new Vector2(1f, 1f), rect.size - new Vector2(2f, 2f)), Color.white);
+                }
+                GUI.DrawTextureWithTexCoords(new Rect(rect.position + new Vector2(2f, 2f), rect.size - new Vector2(4f, 4f)), tileMap.TextureAtlas, spriteRect);
             }
+
+            GUILayout.FlexibleSpace();
+            EditorGUILayout.EndHorizontal();
         }
     }
 }
