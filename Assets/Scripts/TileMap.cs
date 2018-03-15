b7539ac0c17292f05205b21624048d21e4943441
Julian Bath <julzso23@gmail.com>
2018-03-15 20:33:15 +0000

    Rendered grid gizmo

--- Assets/Scripts/TileMap.cs
+++ Assets/Scripts/TileMap.cs
@@ -12,4 +12,22 @@ public class TileMap : MonoBehaviour
             return textureAtlas;
         }
     }
+
+    private void OnDrawGizmos()
+    {
+        DrawGrid(-1000f, 1000f, 1f);
+    }
+
+    private void DrawGrid(float min, float max, float cellSize)
+    {
+        for (float x = min; x < max; x += cellSize)
+        {
+            Gizmos.DrawLine(new Vector2(x, min), new Vector2(x, max));
+        }
+
+        for (float y = min; y < max; y += cellSize)
+        {
+            Gizmos.DrawLine(new Vector2(min, y), new Vector2(max, y));
+        }
+    }
 }
