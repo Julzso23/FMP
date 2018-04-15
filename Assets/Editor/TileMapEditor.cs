514508a8623bbef138f3dff4016fdf7f10f1d153
Julian Bath <julzso23@gmail.com>
2018-04-15 13:09:43 +0100

    Moved layer baking code into new TileMapBaking class

--- Assets/Editor/TileMapEditor.cs
+++ Assets/Editor/TileMapEditor.cs
@@ -179,7 +179,7 @@ public class TileMapEditor : Editor
                 Transform layerTransform = GetLayerTransform(tileMap, layers.layers[i].name);
                 Vector2 layerPosition;
 
-                tileMap.LayerTextures[i] = BakeLayer(layerTransform, out layerPosition);
+                tileMap.LayerTextures[i] = TileMapBaking.BakeLayerTexture(layerTransform, out layerPosition);
                 tileMap.LayerSprites[i] = Sprite.Create(
                     tileMap.LayerTextures[i],
                     new Rect(
@@ -207,82 +207,6 @@ public class TileMapEditor : Editor
                 }
             }
         }
-    }
-
-    private Texture2D BakeLayer(Transform layer, out Vector2 offset)
-    {
-        Rect rect = new Rect(Vector2.zero, Vector2.zero);
-
-        foreach (Transform tile in layer)
-        {
-            Sprite sprite = tile.GetComponent<SpriteRenderer>().sprite;
-
-            if (tile.position.x * sprite.rect.width < rect.xMin)
-            {
-                rect.xMin = tile.position.x * sprite.rect.width;
-            }
-            if (tile.position.y * sprite.rect.height > rect.yMax)
-            {
-                rect.yMax = tile.position.y * sprite.rect.height;
-            }
-
-            if ((tile.position.x + 1f) * sprite.rect.width > rect.xMax)
-            {
-                rect.xMax = (tile.position.x + 1f) * sprite.rect.width;
-            }
-            if ((tile.position.y - 1f) * sprite.rect.height < rect.yMin)
-            {
-                rect.yMin = (tile.position.y - 1f) * sprite.rect.height;
-            }
-        }
-
-        rect.x = rect.xMin;
-        rect.y = rect.yMax;
-        offset = rect.position / 32f;
-
-        Texture2D texture = new Texture2D(Mathf.FloorToInt(rect.width), Mathf.FloorToInt(rect.height), TextureFormat.ARGB32, false);
-        texture.filterMode = FilterMode.Point;
-
-        // Set the background to transparent
-        for (int x = 0; x < texture.width; x++)
-        {
-            for (int y = 0; y < texture.height; y++)
-            {
-                texture.SetPixel(x, y, Color.clear);
-            }
-        }
-
-        texture.Apply();
-
-        foreach (Transform tile in layer)
-        {
-            Sprite sprite = tile.GetComponent<SpriteRenderer>().sprite;
-
-            const int element = 0;
-            const int mip = 0;
-
-            Vector2 destinationPosition = new Vector2(
-                tile.position.x * sprite.rect.width - rect.xMin,
-                texture.height + ((tile.position.y - 1f) * sprite.rect.height - rect.y)
-            );
-
-            Graphics.CopyTexture(
-                sprite.texture,                            // Source texture
-                element,                                   // Source element
-                mip,                                       // Source mip level
-                Mathf.FloorToInt(sprite.rect.xMin),        // Source X
-                Mathf.FloorToInt(sprite.rect.yMin),        // Source Y
-                Mathf.FloorToInt(sprite.rect.width),       // Source width
-                Mathf.FloorToInt(sprite.rect.height),      // Source height
-                texture,                                   // Destination texture
-                element,                                   // Destination element
-                mip,                                       // Destination mip level
-                Mathf.FloorToInt(destinationPosition.x),   // Destination X
-                Mathf.FloorToInt(destinationPosition.y)    // Destination Y
-            );
-        }
-
-        return texture;
     }
 
     private void OnSceneGUI()
