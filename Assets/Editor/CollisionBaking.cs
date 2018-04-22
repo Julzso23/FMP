d4cf8bfbbffdf3b2945f7c013ddbaf7b3e2a5f80
Julian Bath <julzso23@gmail.com>
2018-04-22 23:03:19 +0100

    Added functionality to check if two corners are connected

--- Assets/Editor/CollisionBaking.cs
+++ Assets/Editor/CollisionBaking.cs
@@ -37,14 +37,67 @@ public static class CollisionBaking
 
     private static bool IsCorner(Vector2 position, Transform layer)
     {
+        int adjacentCount = GetAdjacentTileCount(position, layer);
+
+        if (adjacentCount == 1 || adjacentCount == 3)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private static int GetAdjacentTileCount(Vector2 position, Transform layer)
+    {
         int adjacentCount = 0;
+
         if (HasTile(layer, position + new Vector2(-1f, 1f))) adjacentCount++;
         if (HasTile(layer, position + Vector2.up)) adjacentCount++;
         if (HasTile(layer, position + Vector2.left)) adjacentCount++;
         if (HasTile(layer, position)) adjacentCount++;
 
-        if (adjacentCount == 1 || adjacentCount == 3)
+        return adjacentCount;
+    }
+
+    private static bool IsConnected(Vector2 position1, Vector2 position2, Transform layer)
+    {
+        if ((position1.x != position2.x) && (position1.y == position2.y))
         {
+            if (position1.x > position2.x)
+            {
+                Vector2 temp = position2;
+                position2 = position1;
+                position1 = temp;
+            }
+
+            for (float x = position1.x + 1; x < position2.x; x++)
+            {
+                if (GetAdjacentTileCount(new Vector2(x, position1.y), layer) != 2)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        if ((position1.x == position2.x) && (position1.y != position2.y))
+        {
+            if (position1.y > position2.y)
+            {
+                Vector2 temp = position2;
+                position2 = position1;
+                position1 = temp;
+            }
+
+            for (float y = position1.y + 1; y < position2.y; y++)
+            {
+                if (GetAdjacentTileCount(new Vector2(position1.x, y), layer) != 2)
+                {
+                    return false;
+                }
+            }
+
             return true;
         }
 
